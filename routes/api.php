5d269520d63b8ecdc2c39ae3805d5e331384f83d
fhianiemae <fhianiemae@gmail.com>
2025-09-07 21:35:59 +0800

    modified routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,21 @@
 
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TransactionController;
 
-Route::get('/test', function() {
-    return response()->json(['message' => 'API works!']);
-});
+// Route::get('/test', function() {
+//     return response()->json(['message' => 'API works!']);
+// });
+
+
+
+// Route::post('/register', [AuthController::class, 'register']);
+// Route::post('/login', [AuthController::class, 'login']);
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::post('/logout', [AuthController::class, 'logout']);
+//     Route::apiResource('categories', CategoryController::class);
+//     Route::apiResource('transactions', TransactionController::class);
+// });
